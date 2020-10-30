d7c9fefbfb6f7aa05a887b9399b191b7809af0a5
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-30 20:52:45 +0900

    細かな修正、ホバーエフェクト実装

--- components/Skills.js
+++ components/Skills.js
@@ -3,19 +3,19 @@ import styled from "styled-components"
 export default () => {
   return (
     <Div>
-      <h1>My Skills</h1>
-        <Ul>
-          <Li>React <Img src="/react.svg"/></Li>
-          <Li>React Hooks <Img src="/hooks.svg"/></Li>
-          <Li>React Redux <Img src="/redux.svg"/></Li>
-          <Li>Nextjs <Img src="/nextjs-3.svg"/></Li>
-          <Li>JavaScript <Img src="/javascript.svg"/></Li>
-          <Li>Firebase <Img src="/firebase-1.svg"/></Li>
-          {/* <Li>AWS <Img src="/aws-logo.svg"/></Li> */}
-          <Li>Styled components <Img src="/styled.png"/></Li>
-          {/* <Li>TypeScript <Img src="/typescript.svg"/></Li> */}
-          {/* <Li>Python <Img src="/python-4.svg"/></Li> */}
-      </Ul>      
+      <h1>My Skills<span>（Not master）</span></h1>
+      <Ul>
+        <Li>React <Img src="/react.svg" /></Li>
+        <Li>React Hooks <Img src="/hooks.svg" /></Li>
+        <Li>React Redux <Img src="/redux.svg" /></Li>
+        <Li>Next.js <Img src="/nextjs-3.svg" /></Li>
+        <Li>JavaScript <Img src="/javascript.svg" /></Li>
+        <Li>Firebase <Img src="/firebase-1.svg" /></Li>
+        <Li>AWS <Img src="/aws-logo.svg"/></Li>
+        <Li>Styled components <Img src="/styled.png" /></Li>
+        <Li>TypeScript <Img src="/typescript.svg"/></Li>
+        <Li>Python <Img src="/python-4.svg"/></Li>
+      </Ul>
     </Div>
   )
 }
@@ -24,8 +24,10 @@ const Div = styled.div`
   outline:blue solid 3px;
   width:100%;
   h1 {
-  text-align:center;
-
+    text-align:center;
+    span{
+      font-size:10px;
+  }
 }
 `
 
@@ -36,7 +38,7 @@ const Ul = styled.div`
   display:flex;
   list-style:none;
   flex-direction: row;
-  flex-wrap: wrap;         /*折り返し有り・複数行*/
+  flex-wrap: wrap;        
 `
 
 const Li = styled.div`
@@ -48,8 +50,11 @@ const Li = styled.div`
   flex-direction:column;
   align-items:center;
   justify-content:space-between;
-
-
+  transition: opacity .3s;
+  :hover {
+    opacity:.5;
+    background-color:blue;
+  }
 `
 
 const Img = styled.img`
