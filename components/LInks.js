9156767e88731cdb1ca3f5435c026fe659b23a18
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-11-02 20:41:58 +0900

    fixed bug for deploy

--- components/LInks.js
+++ components/LInks.js
@@ -1,5 +1,6 @@
-import { Fragment } from "react"
-import styled from "styled-components"
+import { Fragment } from 'react';
+import styled from 'styled-components';
+import NextLink from 'next/link';
 
 export default () => {
   return (
@@ -7,39 +8,31 @@ export default () => {
       <Div>
         <h1>Links</h1>
         <A href="https://github.com/kazu-S-1110">My-github-repo</A>
-
-
-
       </Div>
     </>
-  )
-
-
-}
-
+  );
+};
 
 const Div = styled.div`
-  color:blue;
-  margin-top:60px;
-  outline:blue solid 3px;
-  width:100%;
-  display:flex;
-  justify-content:center;
-  align-items:center;
+  color: blue;
+  margin-top: 60px;
+  outline: blue solid 3px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
-  background:#EFEEF4 ;
+  background: #efeef4;
 
-
-  h1{
-    color:black;
+  h1 {
+    color: black;
   }
-
-`
+`;
 
 const A = styled.a`
-  font-size:30px;
-  font-weight:bold;
+  font-size: 30px;
+  font-weight: bold;
   :hover {
-    text-decoration:underline;
+    text-decoration: underline;
   }
-`
+`;
