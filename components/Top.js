12fa90576784358b564e50935bd33e7337932487
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-11-04 17:04:32 +0900

    fixed typo

--- components/Top.js
+++ components/Top.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import Typewriter from 'typewriter-effect'
+import styled from 'styled-components';
+import Typewriter from 'typewriter-effect';
 
 export default () => {
   return (
@@ -7,32 +7,32 @@ export default () => {
       <Con>
         <Typewriter
           options={{
-            strings: ['Hello World', "Com'on World"],
+            strings: ['Hello World', "C'mon World"],
             autoStart: true,
             loop: true,
-            delay: 100
+            delay: 100,
           }}
         />
       </Con>
     </>
-  )
-}
+  );
+};
 
 const Con = styled.div`
-  background-image:url("/pc.jpg");
-  background-color:rgba(255,255,255,0.6);
-  background-blend-mode:lighten;
-  width:100%;
-  height:400px;
+  background-image: url('/pc.jpg');
+  background-color: rgba(255, 255, 255, 0.6);
+  background-blend-mode: lighten;
+  width: 100%;
+  height: 400px;
   justify-content: center;
   align-items: center;
-  display:flex;
-  font-size:50px;
-  color:#00eeff;
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  margin-top:auto;  
-  text-shadow: 0.02em 0.02em 0.02em rgba(0,0,0,1);
+  display: flex;
+  font-size: 50px;
+  color: #00eeff;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  margin-top: auto;
+  text-shadow: 0.02em 0.02em 0.02em rgba(0, 0, 0, 1);
   @media screen and (max-width: 690px) {
-    height:250px;
+    height: 250px;
   }
-`
+`;
