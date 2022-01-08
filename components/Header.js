d9d9611905359c3e1eb658ea6579edc00e58dc8c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-01-08 17:41:15 +0900

    タイトルをChakraに変更。

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
-import { Box, Image, Link, Spacer } from '@chakra-ui/react';
+import { Box, Image, Link, Heading } from '@chakra-ui/react';
 import styled from 'styled-components';
 
 export const Header = () => {
   return (
     <Head>
-      <Title>My Portfolio</Title>
+      <Heading
+        fontSize={{ base: '2xl', md: '5xl' }}
+        color="white"
+        alignSelf="center"
+        fontFamily="serif"
+        ml="5"
+      >
+        My Portfolio
+      </Heading>
       <Box alignSelf="center" display="flex" min-height="17px">
         <Link href="https://github.com/kazu-S-1110/" isExternal mr={'3'}>
           <Image src="./github-icon-1.svg" boxSize="40px" m="2px" />
@@ -18,7 +26,7 @@ export const Header = () => {
 };
 
 const Head = styled.div`
-  width: 100vw;
+  width: 100%;
   background-color: blue;
   background-image: linear-gradient(62deg, blue 0%, #6df7e7 60%);
   top: 0;
@@ -31,13 +39,4 @@ const Head = styled.div`
   /* @media screen and (max-width: 690px) {
     height: auto;
   } */
-`;
-const Title = styled.h1`
-  font-size: 50px;
-  font-family: 'Playfair Display', serif;
-  display: inline;
-  color: white;
-  @media screen and (max-width: 690px) {
-    font-size: 30px;
-  }
 `;
