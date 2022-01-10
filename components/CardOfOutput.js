a4a1526819fe3e23542f6d151fe2fae0864d67b2
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-01-11 08:11:59 +0900

    PopOver機能を実装。

--- components/CardOfOutput.js
+++ components/CardOfOutput.js
@@ -3,24 +3,42 @@ import {
   Button,
   Collapse,
   Link,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverTrigger,
   Text,
   useDisclosure,
 } from '@chakra-ui/react';
 
 export const CardOfOutput = ({ output }) => {
-  const { isOpen, onToggle } = useDisclosure();
-
   return (
     <>
       <Box bg="#AED6FB" borderRadius="xl">
-        <Text textAlign="center" onClick={onToggle}>
-          {output.fields.name}
-        </Text>
-        <Link href={output.fields.url}>
-          <Button>Url</Button>
-        </Link>
+        <Popover>
+          <PopoverTrigger>
+            <Text
+              textAlign="center"
+              cursor="pointer"
+              fontSize={{ base: '20px', md: '24px' }}
+            >
+              {output.fields.name}
+            </Text>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>{output.fields.description}</PopoverBody>
+          </PopoverContent>
+          {output.fields.url && (
+            <Link href={output.fields.url} isExternal>
+              <Button cursor="pointer">Url</Button>
+            </Link>
+          )}
+        </Popover>
       </Box>
-      <Collapse in={isOpen}>{output.fields.description}</Collapse>
     </>
   );
 };
